95abee0e63169373b351053a448a21526ade0d16
c330s <c330s@ymail.com>
2019-04-16 17:37:32 +0200

    Update mby_is_new_post.php

--- mby_is_new_post.php
+++ mby_is_new_post.php
@@ -1,6 +1,9 @@
 <?php 
-// tested with:   WP 5.1.1
-// task:          Check if this screen is a new post (= never saved)
+// tested with:     WP 5.1.1
+// task:            Check if this screen is a new post (= never saved).
+//
+// arguments:       –
+// returns:         true, if file is a new post
 
 function mby_is_new_post() {
     $this_screen = get_current_screen();
@@ -10,5 +13,5 @@ function mby_is_new_post() {
 // check if this screen is a new post (= never saved)
 $is_new = ( mby_is_new_post() ? true : false );
 
-// Reference: 
+// References: 
 // https://codex.wordpress.org/Function_Reference/get_current_screen
