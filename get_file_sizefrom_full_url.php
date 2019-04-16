8b644dbbafecd8cadd04f9ffea076e0ab4203256
c330s <c330s@ymail.com>
2019-04-16 16:28:36 +0200

    Update get_file_sizefrom_full_url.php

--- get_file_sizefrom_full_url.php
+++ get_file_sizefrom_full_url.php
@@ -1,14 +1,14 @@
 <?php
+
+// $file_url is the full url
 function get_file_size_from_full_url( $full_url ) {
-    // $file_url is the full url
     // Get the path from the full url:
     $full_url = parse_url( $full_url, PHP_URL_PATH);
     // remove the leading /
     $full_url = ltrim( $full_url, '/' );
-    // get the file size in byte 
-    // and make it human readable with size_format()
+    // filesize():      get the file size in byte 
+    // size_format():   make it human readable 
     return size_format( filesize( $full_url ), 1 );
-    //
     // For info on parse_url see:
     // https://www.php.net/manual/de/function.parse-url.php
 }
