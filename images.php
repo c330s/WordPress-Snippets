03b509dbab66b9bc70c4b3e01cef2ce6be4daec5
c330s <c330s@ymail.com>
2019-04-16 17:32:54 +0200

    Update images.php

--- images.php
+++ images.php
@@ -5,7 +5,7 @@
 // arguments:       int     $img_id     (the image ID)
 // returns:         string  (= complete alt attribute with leading space; attribute value = alt-text )
 
-function get_alt_tag_from_image_id( $img_id ) {
+function mby_get_alt_tag_from_image_id( $img_id ) {
     $alt = '';
     $alt = get_post_meta( $img_id, '_wp_attachment_image_alt', true );
     if( $alt != '' ) {
